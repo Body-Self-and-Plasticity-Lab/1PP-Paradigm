f211a2afd2cc27ce9e152a4156f3a2394be931a7
marteroel <marteroel@gmail.com>
2017-11-02 14:47:25 +0100

    included questionaires and switching between scenes

---
+++ Full body - Unity/Assets/Scripts/DurationManagerVR.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DurationManagerVR : MonoBehaviour {
+
+
+	public float conditionDuration;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Time.timeSinceLevelLoad >= conditionDuration) 
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+		
+	}
+}
