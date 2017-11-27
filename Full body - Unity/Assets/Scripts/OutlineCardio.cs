540eb867ade8feac06747eb35b46643c6f50f2ce
Body-self-and-plasticity-Lab <33545780+BSP-Lab@users.noreply.github.com>
2017-11-27 12:32:57 +0100

    Added outline colors for visuocardiac mapping

---
+++ Full body - Unity/Assets/Scripts/OutlineCardio.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using cakeslice;
+
+public class OutlineCardio : MonoBehaviour {
+
+	private float lastBeat = 0;
+	private float maxValue;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		string[] arduinoValues = ArduinoControl.arduinoSplitValues;
+		float currentValue = float.Parse(arduinoValues[0]);
+
+		Color c = GetComponent<OutlineEffect>().lineColor0;
+
+		if (currentValue >= maxValue && currentValue < 5)
+			maxValue = currentValue;
+
+		c.a = Mathf.Clamp01 (currentValue / maxValue);
+
+		GetComponent<OutlineEffect>().lineColor0 = c;
+		GetComponent<OutlineEffect>().UpdateMaterialsPublicProperties();
+
+		LogBPM (arduinoValues);
+
+	}
+
+	private void LogBPM (string[] arduino) {
+		
+		if (arduino[1] == "1" && ((Time.time - lastBeat) > 0.1)) { //logs current BPM
+
+			float currentBPM = 60 / (Time.time - lastBeat);
+			Debug.Log ("The current BPM value is " + currentBPM);
+			lastBeat = Time.time;
+		}
+	}
+}
