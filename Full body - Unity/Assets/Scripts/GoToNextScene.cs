1784c83e23115fd9c95e6ef3a2445cc334ef2126
Body-self-and-plasticity-Lab <33545780+BSP-Lab@users.noreply.github.com>
2018-01-26 19:16:49 +0100

    Added instructions for experiment, questionnaires and corrected vas

---
+++ Full body - Unity/Assets/Scripts/GoToNextScene.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToNextScene : MonoBehaviour {
+
+	public void OnNext () {
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+	}
+}
